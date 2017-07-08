ab27fe978f3cd5caa8659f20877d2e9091db8269
Thomas J. Galazka <tjgalazka@gmail.com>
2017-07-08 01:13:59 -0400

    Initial project creation.
    Provides a single Greeting service in the API.

---
+++ FactFinder/Controllers/GreetingController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FactFinder.Controllers
+{
+    /// <summary>
+    /// Provides a simplistic greeting interface.
+    /// </summary>
+    public class GreetingController : ApiController
+    {
+        /// <summary>
+        /// Responnds with a simple greeting message.
+        /// </summary>
+        /// <returns>
+        /// {
+        ///     "greeting": "Hello, World!"
+        /// }
+        /// </returns>
+        [HttpGet]
+        public IHttpActionResult GetGreeting(string name = "")
+        {
+           GreetingDTO greeting = new GreetingDTO()
+            {
+                greeting = String.Format("Hello, {0}!", name.Any() ? name : "World") 
+            };
+            return Ok(greeting);
+        }
+
+        private class GreetingDTO
+        {
+            public string greeting { get; set; }
+        }
+    }
+}
---
+++ FactFinder/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FactFinder.WebApiApplication" Language="C#" %>
